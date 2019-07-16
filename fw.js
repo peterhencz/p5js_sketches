ee48bb5365d6f11c90b2f5a79bdd1bbe467b129d
Peter Hencz <peter.hencz@gmail.com>
2019-07-16 17:49:30 +0200

    save

--- fw.js
+++ fw.js
@@ -1,32 +1,65 @@
-let angle = 0;
+let angle = 1;
 let x = 100;
 let y = 0;
+let c, bgc;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight, WEBGL);
   angleMode(DEGREES);
 }
 
-function draw() {
-  background("#f4baba");
-  push();
-  translate(10, 10);
-  rotate(angle);
-  stroke(20);
-  line(x, y, 0, 0);
-  translate(x, y);
-  rotate(angle);
-  stroke("white");
-  line(0, 0, x, y);
-  translate(x, y);
-  rotate(angle);
-  stroke("#444");
-  line(0, 0, x, y);
-  translate(x, y);
-  rotate(angle);
-  stroke("#444");
-  line(0, 0, x, y);
-  pop();
+function cica() {
+  console.log("cica");
+  fill(200, 100, 30);
+  ellipse(30, 40, 30);
+}
 
+function draw() {
+  noFill();
+  push();
+  rotate(angle / 10);
+  translate(-80, -100);
+  bgc = color(180);
+  background(bgc);
+  for (i = 5; i <= 40; i++) {
+    c = color(2 + i * 5, 50, 100);
+
+    rotateX(angle / 100);
+    stroke(c);
+    translate(i / 80, i / 5);
+    rotateY(angle / 50);
+    curve(
+      200,
+      10 * i,
+      40,
+      80,
+      5 * i,
+      -4 * i,
+      -50,
+      i*4,
+      200,
+      60 * i,
+      -600,
+      -34,
+      300 * i,
+      40, 40, i*10
+    );
+  }
+
+  console.log(x, y);
+  // translate(10, 10);
+  // translate(x, y);
+  // rotate(angle);
+  // stroke("white");
+  // line(0, 0, x / 2, y / 2);
+  // translate(x / 2, y / 2);
+  // rotate(angle);
+  // stroke("#444");
+  // line(0, 0, x / 3, y / 3);
+  // translate(x / 3, y / 3);
+  // rotate(angle);
+  // stroke("#444");
+  // line(0, 0, x, y);
+  pop();
   angle = angle + 1;
 }
